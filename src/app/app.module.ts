e2dde72c6f71473e30bf780d2c23f19ec23013af
PedroT111 <pedrochincolla11@gmail.com>
2022-09-26 18:42:52 -0300

    configuration repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DealerComponent } from './components/dealer/dealer.component';
+import { PlayerComponent } from './components/player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DealerComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
